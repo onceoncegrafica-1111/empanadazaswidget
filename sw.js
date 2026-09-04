b7fad65ef0abae3338f244026d38f86b81e2a723
onceoncegrafica-1111 <onceonce.grafica@gmail.com>
2026-09-04 18:50:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,11 @@
-const CACHE_NAME = 'empanadazas-v2';
-const ASSETS = [
-  './',
-  './index.html',
-  './app.html',
-  './manifest.json',
-  './empanadazas-icon.png'
-];
-
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((k) => {
-          if (k !== CACHE_NAME) return caches.delete(k);
-        })
-      );
-    }).then(() => clients.claim())
-  );
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
